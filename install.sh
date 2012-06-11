6bf6776b9d76f95a2e8176f74f209e9f49dc8ae6
Jani Luostarinen <jani.luostarinen@gmail.com>
2012-06-11 09:54:45 +0300

    Init commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "initializing submodules"
+git submodule init
+git submodule update
+
+echo "Deleting the old files"
+rm ~/.vimrc
+rm ~/.zshrc
+rm -rf ~/.vim
+
+echo "Symlinking files"
+ln -s ~/GIT/dotfiles/vimrc ~/.vimrc
+ln -s ~/GIT/dotfiles/zshrc ~/.zshrc
+ln -s ~/GIT/dotfiles/vim ~/.vim
+
+cp ~/GIT/dotfiles/oh-my-zsh/themes/ ~/.oh-my-zsh/themes/jluostar.zsh-theme 
---
+++ oh-my-zsh/themes/jluostar.zsh-theme
@@ -0,0 +1,9 @@
+# Yay! High voltage and arrows!
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[green]%}["
+ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}⚡%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+PROMPT='%{$fg[magenta]%}[%0~] $(git_prompt_info) %{$reset_color%} '
+
